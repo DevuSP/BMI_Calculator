038b34c89053a12453464e6c7fb8a7c47e64bda9
Devashish Singh Parmar <parmardevashish132@gmail.com>
2023-08-12 20:18:26 +0530

    Changing floor to round for more accurate bmi.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function calculateBmi() {
     if (weight === "" || height == "") { return } 
     else {
         // Changing cm in meters.
-        let bmi = Math.floor(weight / (height * height/10000));
+        let bmi = Math.round(weight / (height * height/10000));
         if (bmi > 30) { document.getElementById("result").innerHTML = `Your BMI is <span class="span-red">` + bmi + `</span>.` }
         else {
             document.getElementById("result").innerHTML = "Your BMI is " + bmi + "."
